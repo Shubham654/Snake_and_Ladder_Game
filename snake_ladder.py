01957768fa654a288f9f27adf8b828668e9a4704
Shubham Chambhare <Shubhamchambhare654@gmail.com>
2021-06-29 17:33:22 +0530

    Updated emoji

--- snake_ladder.py
+++ snake_ladder.py
@@ -6,19 +6,30 @@ pos1 = 0
 
 def move(pos):
     dice=random.randint(1,6)
-    print(f"Dice:{dice}")
+    if dice == 1:
+        print(f"Dice:{dice} \u2680")
+    elif dice == 2:
+        print(f"Dice:{dice} \u2681")
+    elif dice == 3:
+        print(f"Dice:{dice} \u2682")
+    elif dice == 4:
+        print(f"Dice:{dice} \u2683")
+    elif dice == 5:
+        print(f"Dice:{dice} \u2684")
+    else:
+        print(f"Dice:{dice} \u2685")
     temp_pos = pos
     pos = pos + dice
     if pos in snake:
-       print("Bitten by snake")
+       print("Bitten by snake \U0001F40D")
        pos = snake[pos]
        print(f"Position:{pos}")
     elif pos in ladder:
-        print("Climbed by ladder")
+        print("Climbed by ladder #")
         pos = ladder[pos]
         print(f"Position:{pos}")
     elif pos > 100:
-        print("Invalid move try again.")
+        print("Invalid move try again. \u274C")
         pos = temp_pos
         print(f"Position:{pos}")
     else:
@@ -30,5 +41,5 @@ while True:
     A = input("Press \"ENTER\" to throw dice ")
     pos1 = move(pos1)
     if pos1 == 100:
-        print("Cobratulations you WON!")
+        print("Cobratulations you WON! \U0001F3C6")
         break
